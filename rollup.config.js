e9d016cd898d83c1bc74844af3ccabe65cee917b
dangreen <danon0404@gmail.com>
2022-02-18 15:54:53 +0700

    refactor: new ts options

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-default-export */
 import swc from 'rollup-plugin-swc'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import nodeEsm from '@trigen/browserslist-config/node-esm'
