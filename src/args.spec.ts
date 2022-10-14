d00ba6d52291c685d832954aac2ef3b2d35464a5
Dan Onoshko <danon0404@gmail.com>
2022-10-15 02:52:50 +0700

    test: move to vitest (#317)

--- src/args.spec.ts
+++ src/args.spec.ts
@@ -1,3 +1,4 @@
+import { describe, it, expect } from 'vitest'
 import { alias, option } from './args'
 
 describe('args', () => {
--- src/options.spec.ts
+++ src/options.spec.ts
@@ -1,3 +1,4 @@
+import { describe, it, expect } from 'vitest'
 import { setArgs, argv } from './argv'
 import { option } from './args'
 import { readOptions } from './options'
